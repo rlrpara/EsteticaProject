3702ddf75603b2997eed80fe246d32e03641e50b
Rodrigo L. Ribeiro <rodrigo.ribeiro@questores.com.br>
2022-10-10 13:41:54 -0300

    ajustes

---
+++ src/SempreDivas.Api/Controllers/UsuarioController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SempreDivas.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsuarioController : ControllerBase
+    {
+        #region [Propriedades Privadas]
+
+        #endregion
+
+        [HttpGet("")]
+        public IActionResult ObterTodos() => Ok("");
+    }
+}

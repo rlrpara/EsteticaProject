b491f776af86b802f8faa28664a7da1437191477
Rodrigo L. Ribeiro <rodrigo.ribeiro@questores.com.br>
2022-11-04 16:38:05 -0300

    ajustes

--- tests/VendaFacil.Tests/InfraTests/DataTests/ContextsTests/GeradorDapperBaseTests.cs
+++ tests/VendaFacil.Tests/InfraTests/DataTests/ContextsTests/GeradorDapperBaseTests.cs
@@ -7,11 +7,11 @@ namespace VendaFacil.Tests.InfraTests.DataTests.ContextsTests
     {
         public Empresa ObterEmpresa() => new Empresa
         {
-            Nome = "Estética Malato",
-            CpfCnpj = "430.919.032-49",
-            Telefone = "(49) 99945-8768",
-            Email = "rosemalato@gmail.com",
-            Endereco = "R. Brasília, 187-d, nº 105 - Jardim Itália, Chapecó - SC, 89802-320",
+            Nome = "Estética Teste",
+            CpfCnpj = "386.485.813-54",
+            Telefone = "(69) 98887-3627",
+            Email = "fulanoo@gmail.com",
+            Endereco = "Rua Dom Helder Câmara, 465, Belo Horizonte/MG - 31870-440",
             ValorMensal = 70,
             DataPagamento = 10,
             DataCadastro = Convert.ToDateTime("2022-10-18 23:25:45"),
@@ -24,7 +24,7 @@ namespace VendaFacil.Tests.InfraTests.DataTests.ContextsTests
             var sqlPesquisa = new StringBuilder();
 
             sqlPesquisa.AppendLine($"INSERT INTO EMPRESA (NOME, CPF_CNPJ, TELEFONE, EMAIL, ENDERECO, VALOR_MENSAL, DATA_PAGAMENTO, DATA_CADASTRO, DATA_ATUALIZACAO, ATIVO)");
-            sqlPesquisa.AppendLine($"                     VALUES ('ESTETICA MALATO', '43091903249', '49999458768', 'rosemalato@gmail.com', 'R. BRASILIA, 187-D, N 105 - JARDIM ITALIA, CHAPECO - SC, 89802-320', 70, 10, '2022-10-18 23:25:45', '2022-10-18 23:25:45', True)");
+            sqlPesquisa.AppendLine($"                     VALUES ('ESTETICA TESTE', '38648581354', '69988873627', 'fulanoo@gmail.com', 'RUA DOM HELDER CAMARA, 465, BELO HORIZONTEMG - 31870-440', 70, 10, '2022-10-18 23:25:45', '2022-10-18 23:25:45', True)");
 
             return sqlPesquisa?.ToString()?.Trim();
         }

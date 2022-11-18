8b46d6cabfb93367dc5a93105b8a3f2e076a4bb9
Rodrigo Ribeiro <rlr.para@gmail.com>
2022-11-17 21:26:15 -0300

    add front

---
+++ src/vendafacil-front/src/App.tsx
@@ -0,0 +1,13 @@
+import Dashboard from "./Pages/Dashboard";
+import GlobalStyle from './Styles/GlobalStyles';
+
+function App() {
+  return (
+    <>
+      <GlobalStyle />
+      <Dashboard />
+    </>
+  );
+}
+
+export default App;

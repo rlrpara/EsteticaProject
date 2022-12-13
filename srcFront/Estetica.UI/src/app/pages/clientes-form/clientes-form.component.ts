7af8d80c5072c04463768a43fd7b9492eb9682a1
Rodrigo Ribeiro <rlr.para@gmail.com>
2022-12-13 19:44:11 -0300

    ajustes

--- srcFront/Estetica.UI/src/app/pages/clientes-form/clientes-form.component.ts
+++ srcFront/Estetica.UI/src/app/pages/clientes-form/clientes-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-clientes-form',
@@ -8,9 +9,25 @@ import { Component, Input, OnInit } from '@angular/core';
 export class ClientesFormComponent implements OnInit {
   @Input() btnText!: string;
 
+  clientesForm!: FormGroup;
+
   constructor() { }
 
   ngOnInit(): void {
+    this.clientesForm = new FormGroup({
+      id: new FormControl(''),
+      nome: new FormControl(''),
+      nascimento: new FormControl(''),
+      cpfcnpj: new FormControl(''),
+      endereco: new FormControl(''),
+      cidade: new FormControl(''),
+      uf: new FormControl(''),
+
+    });
+  }
+
+  submit() {
+    console.log("Enviou o formulario");
   }
 
 }

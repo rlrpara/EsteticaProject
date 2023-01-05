77456bf57528e7d15fd61c0701fc28f96e9d75f0
Rodrigo Ribeiro <rlr.para@gmail.com>
2023-01-05 18:11:53 -0300

    feat: login

--- srcFront/frontend/src/pages/index.tsx
+++ srcFront/frontend/src/pages/index.tsx
@@ -1,13 +1,33 @@
-import Head from 'next/head';
-import styles from '../../styles/Home.module.scss';
+import Head from "next/head";
+import styles from '../../styles/home.module.scss';
+import LogoImg from '../../public/imagens/logo.png';
+import Image from 'next/image';
+import Input from "../components/ui/Input";
+import Button from '../components/ui/Button';
+import Link from 'next/link';
 
-export default function Home() {
+export default function Login() {
   return (
     <div>
       <Head>
-        <title>Ola</title>
+        <title>Estética Malato</title>
       </Head>
-      <h1>Bem Vindo</h1>
+      <div className={styles.containerCenter}>
+        <Image src={LogoImg} alt="Logo Sujeiro Pizzaria" />
+
+        <div className={styles.login}>
+          <form>
+            <Input type="text" placeholder="Digite seu E-Mail" />
+            <Input type="password" placeholder="Digite sua senha" />
+
+            <Button type="submit" loadding={false} >Acessar</Button>
+          </form>
+          
+          <Link href="/signup" className={styles.text}>
+            Não possui uma conta? Cadastre-se
+          </Link>
+        </div>
+      </div>
     </div>
   )
 }

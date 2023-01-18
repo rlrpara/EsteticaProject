7b533084e6947fdb404539f606f9038c257a2c17
Rodrigo Ribeiro <rlr.para@gmail.com>
2023-01-18 14:52:39 -0300

    componentes iniciais

--- srcFront/Estetica-UI/src/app/app-routing.module.ts
+++ srcFront/Estetica-UI/src/app/app-routing.module.ts
@@ -1,7 +1,33 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", pathMatch: "full", redirectTo: "login" },
+  {
+    path: "login",
+    loadChildren: () => import('./pages/login/login-routing.module').then(x => x.LoginRoutingModule)
+  },
+  {
+    path: "cliente",
+    loadChildren: () => import('./pages/cliente/cliente-routing.module').then(x => x.ClienteRoutingModule)
+  },
+  {
+    path: "empresa",
+    loadChildren: () => import('./pages/empresa/empresa-routing.module').then(x => x.EmpresaRoutingModule)
+  },
+  {
+    path: "mocimentacao",
+    loadChildren: () => import('./pages/movimentacao/movimentacao-routing.module').then(x => x.MovimentacaoRoutingModule)
+  },
+  {
+    path: "usuario",
+    loadChildren: () => import('./pages/usuario/usuario-routing.module').then(x => x.UsuarioRoutingModule)
+  },
+  {
+    path: "dashboard",
+    loadChildren: () => import('./pages/dashboard/dashboard-routing.module').then(x => x.DashboardRoutingModule)
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- srcFront/Estetica-UI/src/app/pages/dashboard/dashboard-routing.module.ts
+++ srcFront/Estetica-UI/src/app/pages/dashboard/dashboard-routing.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [
+import { DashboardComponent } from './dashboard/dashboard.component';
 
+const routes: Routes = [
+  { path: '', component: DashboardComponent }
 ];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
-export class ClienteRoutingModule { }
+export class DashboardRoutingModule { }

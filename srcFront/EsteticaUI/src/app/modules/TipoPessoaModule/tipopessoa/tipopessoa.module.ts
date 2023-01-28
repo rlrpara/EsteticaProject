c5fa5bebd2d4906b345610d0aa93811cff5babb6
Rodrigo Ribeiro <rlr.para@gmail.com>
2023-01-28 19:17:30 -0300

    ajustes

---
+++ srcFront/EsteticaUI/src/app/modules/TipoPessoaModule/tipopessoa/tipopessoa.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { TipopessoaRoutingModule } from './tipopessoa-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    TipopessoaRoutingModule
+  ]
+})
+export class TipopessoaModule { }
